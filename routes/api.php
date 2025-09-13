b3abaecd58ef2434ae0713769f7297971cb6ac70
Danilo Alves Lopes <daniloalveslopesdev@gmail.com>
2025-09-13 12:27:55 -0300

    feat: adiciona validações por tipo de vaga

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,22 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 
-Route::get('/users', [UserController::class, 'index']);
+Route::prefix('user')->group(function () {
+    Route::post('/register', [UserController::class, 'register'])->name('user.register');
+    Route::get('/search', [UserController::class, 'search']);
+});
+Route::prefix('company')->group(function () {
+    Route::post('/register', [CompanyController::class, 'register']);
+});
+Route::prefix('vacancy')->group(function () {
+    Route::post('/register', [VacancyController::class, 'register']);
+    Route::get('/{id}', [VacancyController::class, 'show']);
+});
+Route::prefix('application')->group(function () {
+    Route::post('/register', [ApplicationController::class, 'register']);
+});
