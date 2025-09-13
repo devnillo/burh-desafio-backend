558c3b3f59ec8196f74d6a4c2a63c1ad9aba5bbb
Danilo Alves Lopes <daniloalveslopesdev@gmail.com>
2025-09-13 12:26:20 -0300

    feat: implementa cadastro de usuários com validação de CPF e e-mail únicos, e busca de usuário via nome, email, cpf, retornando o usuário e as vagas que o mesmo se candidatou-se

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Helpers\ApiResponse;
+use App\Http\Requests\User\RegisterUserRequest;
+use App\Http\Requests\User\SearchUsersRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function create()
+    public function register(RegisterUserRequest $request)
     {
-        
+        $credentials = $request->validated();
+        $user = User::create($credentials);
+        return ApiResponse::success(new UserResource($user), 'Successfully registered user');
+    }
+    public function search(SearchUsersRequest $request)
+    {
+        $filters = $request->validated();
+        $query = User::query();
+        if ($request->filled('nome')) {
+            $query->where('name', 'like', '%' . $request->nome . '%');
+        }
+        if ($request->filled('email')) {
+            $query->where('email', $request->email);
+        }
+        if ($request->filled('cpf')) {
+            $query->where('cpf', $request->cpf);
+        }
+        $users = $query->with('vacancies')->get();
+
+        return UserResource::collection($users);
     }
 }
