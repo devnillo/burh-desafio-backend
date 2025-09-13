8cb1dd69585a662edffba4441c281fcf6bcde630
Danilo Alves Lopes <daniloalveslopesdev@gmail.com>
2025-09-13 12:18:26 -0300

    feat: cria migrações de empresa, usuário, vaga e candidatura

---
+++ database/migrations/2025_09_11_201523_create_vacancies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('type', ['PJ', 'CLT', 'INTERNSHIP']);
+            $table->decimal('salary', 10, 2)->nullable();
+            $table->integer('working_hours')->nullable();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacancies');
+    }
+};
